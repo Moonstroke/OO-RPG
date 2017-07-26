bec2c949d50dfd64f679b3a413b3c141fe860128
joH1 <jiema@live.fr>
2017-07-26 19:58:19 +0200

    removed attribute courage (now in Mob class), `clone()`, exception from `buffStat()`, methods `fight()` and `flee()` (now in Duellist interface); changed `isChalleangeable()` body

--- src/joH1/oo_rpg/Creature.java
+++ src/joH1/oo_rpg/Creature.java
@@ -17,8 +17,6 @@ public abstract class Creature extends Entity {
 
 	public static final int STAT_SPEED = 0x8;
 
-	public static final int STAT_COURAGE = 0x10;
-
 
 	protected int health;
 
@@ -32,38 +30,17 @@ public abstract class Creature extends Entity {
 
 	protected int speed;
 
-	/**
-	 * The courage of the creature (ie. its ability, in combat, to not be frightened during battle)
-	 * 0 <= courage <= 100
-	 * A courage of 0 means the creature will always try to flee and never attacks
-	 * (like peaceful creatures)
-	 * A courage of 100 means the creature will never flee a battle -- used with players
-	 * (who will decide by themselves to fight or flee) or bosses
-	 */
-	protected int courage;
 
-
-	public Creature(String name, int level, int initialHealth, int force, int defence, int speed, int courage) { // Use this
-		super(name, level);
-		health = maxHealth = initialHealth;
-		alive = true;
-		this.force = force;
-		this.defence = defence;
-		this.speed = speed;
-		this.courage = courage;
-	}
-
-	protected Creature(String name, int level, int health, int maxHealth, boolean alive, int force, int defence, int speed, int courage) {
+	protected Creature(String name, int level, int health, int maxHealth, boolean alive, int force, int defence, int speed) { // Use this
 		super(name, level);
 		this.health = health;
 		this.maxHealth = maxHealth;
 		this.alive = alive;
-		this.alive = alive;
 		this.force = force;
 		this.defence = defence;
 		this.speed = speed;
-		this.courage = courage;
 	}
+
 
 	@Override
 	public boolean equals(Object o) {
@@ -75,14 +52,8 @@ public abstract class Creature extends Entity {
 
 	@Override
 	public String toString() {
-		return String.format("%s [%d/%d], %d / %d -- %d | %d%%", super.toString(), health, maxHealth, force, defence, speed, courage);
+		return String.format("%s [%d/%d], %d / %d -- %d", super.toString(), health, maxHealth, force, defence, speed);
 	}
-
-	@Override
-	public Creature clone() {
-		return new Creature(new String(name), level, health, maxHealth, alive, force, defence, speed, courage);
-	}
-
 
 	public boolean isAlive() {
 		return alive;
@@ -125,9 +96,9 @@ public abstract class Creature extends Entity {
 	 * @param stat   The statistic to increase
 	 * @param amount The value by which to increqse the stat
 	 *
-	 * @return The new value of the statistic
+	 * @return The new value of the statistic or {@code 0} if the stat could not be found
 	 */
-	public int buffStat(int stat, int amount) throws IllegalArgumentException {
+	public int buffStat(int stat, int amount) {
 		switch(stat) {
 			case STAT_HEALTH:
 				return health += amount;
@@ -137,10 +108,8 @@ public abstract class Creature extends Entity {
 				return defence += amount;
 			case STAT_SPEED:
 				return speed += amount;
-			case STAT_COURAGE:
-				return courage = (courage + amount) > 100 ? 100 : courage + amount;
 			default:
-				throw new IllegalArgumentException(stat + " is not a valid stat value");
+				return 0;
 		}
 	}
 
@@ -152,39 +121,24 @@ public abstract class Creature extends Entity {
 	public abstract void strike(Creature c);
 
 	/**
+	 * Run in fear from another creature
+	 *
+	 * @param c The creature to run from
+	 */
+	public abstract void runFrom(Creature c);
+
+	/**
 	 * Can the creature be challenged in duel?
 	 *
 	 * @return {@code true} iff the {@code Challengeable} annotation is present
 	 *         on the creature's end class
 	 */
 	public boolean isChallengeable() {
-		return this.getClass().isAnnotationPresent(Challengeable.class);
+		for(Class<?> i : this.getClass().getInterfaces())
+			if(i.equals(Duellist.class))
+				return true;
+		return false;
 	}
-
-	/**
-	 * Start a combat with another creature.
-	 * Should be overriden for aggressive creatures
-	 *
-	 * @param c The attacked creature
-	 *
-	 * @return a {@link Duel} between the creature and the attacked creature
-	 *         if the latter {@link Challengeable can be challenged},
-	 *         {@code null} otherwise
- 	 */
-	public Duel fight(Creature c) {
-		if(c.isChallengeable())
-			return new Duel(this, c);
-		strike(c);
-		return null;
-	}
-
-	/**
-	 * Run in fear from a creature
-	 * If courage is 0, will always run away, even outside of battle
-	 *
-	 * @param c The creature it runs from
-	 */
-	public abstract void flee(Creature c);
 
 }
 
