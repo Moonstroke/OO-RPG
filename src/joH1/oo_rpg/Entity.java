c42cf5fa09934c3bcab4a0b2fe4ffb86cc375f77
joH1 <jiema@live.fr>
2017-07-26 11:00:13 +0200

    made Entity abstract

--- src/joH1/oo_rpg/Entity.java
+++ src/joH1/oo_rpg/Entity.java
@@ -3,7 +3,7 @@ package joH1.oo_rpg;
 import java.lang.StringBuilder;
 import java.io.Serializable;
 
-public class Entity implements Cloneable, Serializable {
+public abstract class Entity implements Cloneable, Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected String name;
@@ -14,29 +14,8 @@ public class Entity implements Cloneable, Serializable {
 	protected int level;
 
 
-	protected Entity() { // Default ctor
-		name = "";
-		level = 0;
-	}
-
-	protected Entity(Entity e) { // Copy ctor
-		name = new String(e.name);
-		level = e.level;
-	}
-
-	protected Entity(String name, int level) { // Use this
-		this.name = name;
-		this.level = level;
-	}
-
 	@Override
-	public String toString() {
-		// Leaving 7 chars for String representations of `health` and `maxHealth` seems reasonable
-		StringBuilder sb = new StringBuilder(name.length() + 20);
-		sb.append("Entity \"").append(name).append("\" [").append(String.valueOf(health));
-		sb.append('/').append(String.valueOf(maxHealth)).append(']');
-		return sb.toString();
-	}
+	public abstract String toString();
 
 	@Override
 	public boolean equals(Object o) {
@@ -44,11 +23,6 @@ public class Entity implements Cloneable, Serializable {
 			return false;
 		Entity e = (Entity)o;
 		return name.equals(e.name) && level = e.level;
-	}
-
-	@Override
-	public Entity clone() {
-		return new Entity(new String(name), level);
 	}
 
 	/**
@@ -60,5 +34,14 @@ public class Entity implements Cloneable, Serializable {
 		name = newName;
 	}
 
+	/**
+	 * Increases the entity's level.
+	 * This basic implementation should be overriden
+	 *
+	 * @return The new level of the entity
+	 */
+	public int levelUp() {
+		return ++level;
+	}
 }
 
