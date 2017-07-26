c57cbbf8d2c70a23594fcfed6aabed7368cb54c2
joH1 <jiema@live.fr>
2017-07-26 13:18:04 +0200

    added *courage* value ctor parameters

--- src/joH1/oo_rpg/Mob.java
+++ src/joH1/oo_rpg/Mob.java
@@ -4,8 +4,8 @@ package joH1.oo_rpg;
 public class Mob extends Creature {
 	private static final long serialVersionUID = 3L;
 
-	public Mob(String name, int level, int initialHealth, int force, int defence, int speed) {
-		super(name, level, initialHealth, force, defence, speed);
+	public Mob(String name, int level, int initialHealth, int force, int defence, int speed, int courage) {
+		super(name, level, initialHealth, force, defence, speed, courage);
 	}
 
 	@Override
