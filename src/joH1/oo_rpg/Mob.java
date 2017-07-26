b4b9156a93e35e6a6089c54a74f9cd989b3d2f0e
joH1 <jiema@live.fr>
2017-07-26 12:48:21 +0200

    added ctor

--- src/joH1/oo_rpg/Mob.java
+++ src/joH1/oo_rpg/Mob.java
@@ -4,6 +4,9 @@ package joH1.oo_rpg;
 public class Mob extends Creature {
 	private static final long serialVersionUID = 3L;
 
+	public Mob(String name, int level, int initialHealth, int force, int defence, int speed) {
+		super(name, level, initialHealth, force, defence, speed);
+	}
 
 	@Override
 	public void fight(Creature c) {
