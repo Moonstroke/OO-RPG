f2871e52aadca3c49b0acd00ad9a5a28850d94e5
joH1 <jiema@live.fr>
2017-07-26 12:46:09 +0200

    fixed errors: missing type, and invalid field name

--- src/joH1/oo_rpg/Equipment.java
+++ src/joH1/oo_rpg/Equipment.java
@@ -1,14 +1,14 @@
 package joH1.oo_rpg;
 
 public class Equipment extends Item {
-	private static final serialVersionUID = 5L;
+	private static final long serialVersionUID = 5L;
 
 	public Equipment(String name, int level, int category, boolean isLootable) {
 		super(name, level, category, isLootable);
 	}
 
 	public int equip(Creature c) throws IllegalArgumentException {
-		return c.buffStat(cat, level);
+		return c.buffStat(stat, level);
 	}
 }
 
